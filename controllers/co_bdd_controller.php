f75de7b25cacb4ca32182c50cf3bc32ef335e9b3
80010-91-15 <80010-91-15@exchange.ad.afpanet>
2020-01-10 15:53:19 +0100

    jeej

--- controllers/co_bdd_controller.php
+++ controllers/co_bdd_controller.php
@@ -1,5 +1,5 @@
 <?php
-
+// connexion a la base de données
 $db = new PDO('mysql:host=localhost:3307;charset=utf8;dbname=record', 'root', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
